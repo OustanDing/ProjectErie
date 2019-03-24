79fda44d17e52160973b38a97ad5dc531047de08
Oustan <oustan10@gmail.com>
2019-03-24 00:54:15 -0400

    added paths to webpages

--- app.py
+++ app.py
@@ -200,5 +200,17 @@ def delete():
 def index():
 	return render_template('index.html')
 
+@app.route('/map')
+def map():
+	return render_template('map.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
+@app.route('/howto')
+def howto():
+	return render_template('howto.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
